9c719a20481d59e04cff5397b0c46902e3cfcd9a
Thiago Lagden <lagden@gmail.com>
2021-07-02 02:39:07 -0300

    fix label

--- src/base/Label.svelte
+++ src/base/Label.svelte
@@ -7,5 +7,6 @@
 	class="_atom_frm__label {className}"
 	class:_atom_frm__label___disabled={$$restProps?.disabled ?? false}
 	class:_atom_frm__label___required={$$restProps?.required ?? false}
+	class:_atom_frm__label___toggle={$$restProps?.toggleMode ?? false}
 	{...$$restProps}
 ><slot /></label>
